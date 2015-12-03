cd925214a3c6a4fda268a6a78c7999912a5b0b1d
coletta <coletta@debianDev>
2015-12-03 18:38:36 +0100

    Import from github bloodhoud server

--- routes/databaseCon.js
+++ routes/databaseCon.js
@@ -33,13 +33,21 @@ var databaseCon = function(){
 				"db":DB_NAME
 			}
 
-			if(process.env.OPENSHIFT_MONGODB_PASSWORD){
+			if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
 				//on cloud
 				console.log(METHOD + " Deploy on cloud RH Openshift");
-TBC
 
-				var env = JSON.parse(process.env.VCAP_SERVICES);
-				mongo = env['mongodb-1.8'][0]['credentials'];
+                connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+                process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                process.env.OPENSHIFT_APP_NAME;
+
+				mongo.hostname = process.env.OPENSHIFT_MONGODB_DB_HOST;
+				mongo.port = process.env.OPENSHIFT_MONGODB_DB_PORT;
+				mongo.username = process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+				mongo.password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD;
+				mongo.name = process.env.OPENSHIFT_APP_NAME;
 			}
 			else{
 				//on locale
